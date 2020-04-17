e05c6496202e633de731e56935a2250907c8d46f
Joao Felipe <joaocamargo2205@gmail.com>
2020-04-17 17:18:25 -0300

    fragmentNaActivity

--- android/FragBundle/app/src/main/java/com/example/aulafragintent/PerfilActivity.java
+++ android/FragBundle/app/src/main/java/com/example/aulafragintent/PerfilActivity.java
@@ -1,15 +1,21 @@
 package com.example.aulafragintent;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class PerfilActivity extends AppCompatActivity {
 
     private TextView nome, email;
-    private Button btnVoltar;
+    private Button btnVoltar, btnNoticia;
+    private FragmentManager fragmentManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +25,25 @@ public class PerfilActivity extends AppCompatActivity {
         nome = findViewById(R.id.id_nomeperfil);
         email = findViewById(R.id.id_emailperfil);
         btnVoltar = findViewById(R.id.btn_voltar);
+        btnNoticia = findViewById(R.id.btn_noticia);
 
         if(getIntent() != null){
             Bundle bundle = getIntent().getExtras();
             nome.setText(bundle.getString("NOME"));
+            email.setText(bundle.getString("EMAIL"));
         }
+
+        btnNoticia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                carregaFragment(new NoticiaFragment());
+            }
+        });
+    }
+    public void carregaFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.container, fragment);
+        fragmentTransaction.commit();
     }
 }
